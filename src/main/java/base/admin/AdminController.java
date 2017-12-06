a3cfa5d343a0df59450cac7eb6f8780d73cccab1
mario <mario@scalet-rm.de>
2017-12-06 16:19:32 +0100

    Added admin Controller that gives you a session(No Password Authentication yet)

--- src/main/java/base/admin/AdminController.java
+++ src/main/java/base/admin/AdminController.java
@@ -1,31 +1,25 @@
 package base.admin;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import base.activitymeter.Activity;
-import base.activitymeter.ActivityRepository;
-
 @RestController
-@RequestMapping("/delete/{activityID}")
-public class AdminController {
-
-	@Autowired
-	private EMailRepository emailRepository;
-	
-	@Autowired
-	private ActivityRepository activityRepository;
-	
+@RequestMapping("/admin")
+public class AdminController
+{
 	@GetMapping
-	public void deleteActivityBlockEmail(@PathVariable Long activityID) {
-		Activity activityToDelete = activityRepository.findOne(activityID);
-		
-		String eMail = activityToDelete.geteMail();
-		emailRepository.save(new EMail(eMail));
-		
-		activityRepository.delete(activityToDelete);
+	public String admin(HttpSession s)
+	{
+		if(s.getAttribute("login") == null)
+		{
+			s.setAttribute("login", "true");
+
+			return "You are now logged in as addmin";
+		}
+		return "You are already logged in as admin";
 	}
 }
