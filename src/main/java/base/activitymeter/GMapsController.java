3da3ca54b079b0649bca5adcff4659560970c09b
jo-code <jan@ottisolutions.de>
2017-12-06 08:52:09 +0100

    fixed some code smells

--- src/main/java/base/activitymeter/GMapsController.java
+++ src/main/java/base/activitymeter/GMapsController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/mapdata")
@@ -13,7 +14,7 @@ public class GMapsController {
 	private ActivityRepository activityRepository;
 
 	@GetMapping("{uni}")
-	public ArrayList<Activity> find(@PathVariable String uni) {
+	public List<Activity> find(@PathVariable String uni) {
 		ArrayList<Activity> activities = new ArrayList<>();
 
 		for (Activity a : activityRepository.findAll()) {
