860fee7ae5c1bb90ba6832dfa177cce6d8bcfb58
mario <mario@scalet-rm.de>
2017-12-07 12:50:26 +0100

    Added admin page with menu bar

---
+++ src/main/java/base/AdminPageController.java
@@ -0,0 +1,16 @@
+package base;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminPageController {
+
+    @GetMapping
+    public String index() { 
+    	return "admin/index.html"; 
+    }
+
+}
